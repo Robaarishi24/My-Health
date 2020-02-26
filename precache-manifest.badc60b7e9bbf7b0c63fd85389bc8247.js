74519d58e19f497249b48ec9a018a163e5e9fc24
robaarishi24 <robaarishi@hotmail.com>
2020-02-26 16:05:28 +0300

    Updates

---
+++ precache-manifest.badc60b7e9bbf7b0c63fd85389bc8247.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d268aaa3915950bbd66c12c3bfc204b",
+    "url": "/My-Health/index.html"
+  },
+  {
+    "revision": "12f00cac36da7448c708",
+    "url": "/My-Health/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "12ea21a5e3e58f128b7c",
+    "url": "/My-Health/static/css/main.737cdcec.chunk.css"
+  },
+  {
+    "revision": "12f00cac36da7448c708",
+    "url": "/My-Health/static/js/2.e546fc44.chunk.js"
+  },
+  {
+    "revision": "0f3c990d3332ea672c55d229dbc2e342",
+    "url": "/My-Health/static/js/2.e546fc44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12ea21a5e3e58f128b7c",
+    "url": "/My-Health/static/js/main.6e5ed331.chunk.js"
+  },
+  {
+    "revision": "9f573a8c9e9de7cbef63",
+    "url": "/My-Health/static/js/runtime-main.4ea71a08.js"
+  }
+]);
